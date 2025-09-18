95c279f8c1149f36444bc716a57da8c98979776d
JkayJatan <jatankumarjk806@gmail.com>
2025-09-18 14:20:20 +0530

    Add assignment 1

---
+++ 01_basic_calculator.py
@@ -0,0 +1,23 @@
+def calculator():
+    print("----simple calculator----")
+    print("select operation:")
+    print("1.Add(+)")
+    print("2.Subtract(-)")
+    print("3.Multiply(*)")
+    print("4.Division(/)")
+calculator()  
+choice = input("Enter choice(1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if choice == '1':
+    print(num1, "+", num2, "=", num1 + num2)
+elif choice == '2':
+    print(num1, "-", num2, "=", num1 - num2)    
+elif choice == '3':
+    print(num1, "*", num2, "=", num1 * num2)    
+elif choice == '4':
+    if num2 != 0:
+        print(num1, "/", num2, "=", num1 / num2)
+    else:
+        print("Error! Division by zero.")
+                   
